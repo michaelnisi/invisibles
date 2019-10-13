128f89950b671c6a79e5d2d1d6c701a954c17125
Michael Nisi <michael.nisi@gmail.com>
2019-10-13 08:05:29 +0200

    Import repos

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# Useful commands
+# Invisibles
+
+> “Dear Invisible ones!” This is how Walter Benjamin addressed thousands of children through the new medium of German radio between 1927 and 1933.
+
+Invisibles experimentally migrates some services to AWS Fargate using the AWS CDK.
+
+## Useful commands
 
  * `npm run build`   compile typescript to js
  * `npm run watch`   watch for changes and compile
@@ -6,3 +12,7 @@
  * `cdk deploy`      deploy this stack to your default AWS account/region
  * `cdk diff`        compare deployed stack with current state
  * `cdk synth`       emits the synthesized CloudFormation template
+
+## License
+
+[MIT License](https://raw.github.com/michaelnisi/invisibles/master/LICENSE)
--- lib/invisibles-stack.ts
+++ lib/invisibles-stack.ts
@@ -1,9 +1,34 @@
-import cdk = require('@aws-cdk/core');
+import cdk = require('@aws-cdk/core')
+import ecs = require("@aws-cdk/aws-ecs")
+import ecsPatterns = require("@aws-cdk/aws-ecs-patterns")
+import { Repository } from '@aws-cdk/aws-ecr'
+
+function importMangerImage(scope: cdk.Construct): ecs.ContainerImage {
+  return ecs.ContainerImage.fromEcrRepository(
+    Repository.fromRepositoryName(scope, 'MangerRepo', 'manger'), 
+    '0.1.0'
+  )
+}
+
+function importFanboyImage(scope: cdk.Construct): ecs.ContainerImage {
+  return ecs.ContainerImage.fromEcrRepository(
+    Repository.fromRepositoryName(scope, 'FanboyRepo', 'fanboy'), 
+    '0.1.0'
+  )
+}
 
 export class InvisiblesStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
+    new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'Service', {
+      publicLoadBalancer: true,
+      memoryLimitMiB: 1024,
+      cpu: 512,
+      taskImageOptions: {
+        image: importMangerImage(this),
+        containerPort: 3000
+      }
+    })
   }
 }
